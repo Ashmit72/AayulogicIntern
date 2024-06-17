55f0b053198abd7d71d3d97a4786a7f0402a0262
ashmit <ashmit@ashmit.ashmit>
2024-06-17 17:53:00 +0545

    Complete Custom designed web template

---
+++ web-design-principles/officeWebsite/index.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     new Splide('#topsplide', {
+//         type: "loop",
+//         autoplay: false,
+//         perPage: 3,
+//         rewindSpeed: 1,
+//         snap:false,
+//         autoScroll:{
+//          speed:1
+//         },
+//         breakpoints: {
+//           767: {
+//             perPage: 1,
+//           },
+//           992: {
+//             perPage: 2,
+//           },
+//         },
+//     }).mount(window.splide.Extensions);
+//   });
+
+// const arrow=document.querySelector('.splide__arrow')
+// console.log(arrow);
