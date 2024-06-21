915e3d85ffbd176837742af97104434d227d8848
ashmit <ashmit@ashmit.ashmit>
2024-06-21 16:59:56 +0545

    Refactor Office Website

--- web-design-principles/officeWebsite2/index.js
+++ web-design-principles/officeWebsite2/index.js
@@ -1,12 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    new Splide('#testimonial-slider', {
-      type   : 'loop',
-      perPage: 2,
-      autoplay: true,
-      breakpoints: {
-        768: {
-          perPage: 1,
-        },
-      },
-    }).mount();
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     new Splide('#testimonial-slider', {
+//       type   : 'loop',
+//       perPage: 3,
+//       autoplay: true,
+//       breakpoints: {
+//         1382: {
+//           perPage: 2,
+//         },
+//         974: {
+//           perPage: 1,
+//         },
+//       },
+//     }).mount();
+//   });
+
+document.addEventListener( 'DOMContentLoaded', function () {
+            new Splide( '#testimonial-slider', {
+                type       : 'loop',
+                perPage    : 1,
+                autoplay   : false,
+                interval   : 5000,
+                pauseOnHover: true,
+            } ).mount();
+        } );
